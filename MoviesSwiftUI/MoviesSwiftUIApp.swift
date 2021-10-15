72e5836c230488c31948d1dbd9b1a42394f00091
Doğukaan Kılıçarslan <40922278+darkbringer1@users.noreply.github.com>
2021-10-15 03:34:39 +0300

    Initial Commit

---
+++ MoviesSwiftUI/MoviesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesSwiftUIApp.swift
+//  MoviesSwiftUI
+//
+//  Created by DarkBringer on 15.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MoviesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
